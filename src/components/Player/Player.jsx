ad9358e5d69e9f182a1ca8c90603e2a5dd322eed
Anton Pavl <antonpavl96@gmail.com>
2022-03-14 18:29:04 +0200

    created player using react-spotify-web-playback

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import SpotifyPlayer from "react-spotify-web-playback";
 
-const Player = () => {
-  return <div>Player</div>;
+const Player = ({ accessToken, trackUri }) => {
+  if (!accessToken) return null;
+  return (
+    <SpotifyPlayer
+      token={accessToken}
+      showSaveIcon
+      uris={trackUri ? [trackUri] : []}
+    />
+  );
 };
 
 export default Player;
