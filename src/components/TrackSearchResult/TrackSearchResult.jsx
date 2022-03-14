03e8c802f7c32c1116cecc54112f8f985f8a898b
Anton Pavl <antonpavl96@gmail.com>
2022-03-14 18:17:45 +0200

    created TrackSearchResult jsx layout

--- src/components/TrackSearchResult/TrackSearchResult.jsx
+++ src/components/TrackSearchResult/TrackSearchResult.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
-const TrackSearchResult = () => {
-  return <div>TrackSearchResult</div>;
+const TrackSearchResult = ({ track }) => {
+  return (
+    <div
+      className="d-flex m-2 align-items-center"
+      style={{ cursor: "pointer" }}
+    >
+      <img src={track.albumUrl} style={{ height: "64px", width: "64px" }} />
+      <div className="ml-3">
+        <div>{track.title}</div>
+        <div className="text-muted">{track.artist}</div>
+      </div>
+    </div>
+  );
 };
 
 export default TrackSearchResult;
