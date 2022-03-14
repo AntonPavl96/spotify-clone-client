5ccc7b5969c3a066452ed825ef944bdb9f706e8b
Anton Pavl <antonpavl96@gmail.com>
2022-03-14 15:41:43 +0200

    added conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
-import { Login } from "./components";
+import { Login, Dashboard } from "./components";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+const code = new URLSearchParams(window.location.search).get("code");
+
 const App = () => {
-  return <Login />;
+  return doe ? <Dashboard code={code} /> : <Login />;
 };
 
 export default App;
